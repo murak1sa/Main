bd0bfde8ea3b9eb18f99e61f11304d0af558fc73
Ka. / <khagnguyen068@gmail.com>
2025-04-03 22:11:01 +0700

    Create Arceus.user.js

---
+++ Arceus.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        「⚔」Owlus - Arceus
+// @version      1.0
+// @namespace    http://tampermonkey.net/
+// @description  Only Bypass Arceus
+// @author       Owlus Team
+// @run-at       document-end
+// @match        *://*.linkvertise.com/*
+// @match        *://*.linkvertise.net/*
+// @match        *://*.link-to.net/*
+// @match        https://loot-link.com/s?*
+// @match        https://loot-links.com/s?*
+// @match        https://lootlink.org/s?*
+// @match        https://lootlinks.co/s?*
+// @match        https://lootdest.info/s?*
+// @match        https://lootdest.org/s?*
+// @match        https://lootdest.com/s?*
+// @match        https://links-loot.com/s?*
+// @match        https://linksloot.net/s?*
+// @match        https://spdmteam.com/key-system*
+
+// @exclude      *://publisher.linkvertise.com/*
+// @exclude      *://linkvertise.com/search*
+// @exclude      *://linkvertise.com/login*
+// @exclude      *://linkvertise.com/profile*
+// @exclude      *://blog.linkvertise.com
+// @exclude      *://blog.linkvertise.com/*
+// @exclude      *://linkvertise.com/assets/vendor/*
+// @exclude      *://publisher.linkvertise.com/*
+// @exclude      *://link-mutation.linkvertise.com/*
+// @exclude      *://linkvertise.com/assets/external/thinksuggest
+
+// @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
+// @require     https://greasyfork.org/scripts/464780-global-module/code/global_module.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require     https://raw.githubusercontent.com/murak1sa/OwlusDontSaid/refs/heads/main/Yoh.js
+// @require     https://raw.githubusercontent.com/murak1sa/OwlusTeam/refs/heads/main/Arceus.js
+// ==/UserScript==
